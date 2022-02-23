866e88402ec17188cf167afec970337c5e2ef7a2
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-23 20:48:17 +0000

    Upload texto.js

---
+++ src/models/texto.js
@@ -0,0 +1,38 @@
+import {Sequelize} from 'sequelize';
+import sequelize, {Sequelize} from '../database/db';
+import Usuario from './usuario';
+
+const Texto = sequelize.define('texto' ,{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre_seccion: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    tag: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    titulo: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    descripcion: {
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+},{
+    timestamps: false,
+    tableName: 'texto'
+});
+
+
+//Definicion relacion usuario
+
+Texto.belongsToMany (Usuario,{through: "texto_has_usuario"});
+Usuario.belongsToMany(Texto,{through: "texto_has_usuario"});
+
+export default Texto;
