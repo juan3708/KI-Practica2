4ea745e5a6fb937c0b726f4e8b20411bac40b974
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-23 20:45:15 +0000

    Upload bitacora.js

---
+++ src/models/bitacora.js
@@ -0,0 +1,31 @@
+import {Sequelize} from 'sequelize';
+import sequelize, {Sequelize} from '../database/db';
+
+const Bitacora = sequelize.define('bitacora' ,{
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    tabla_referencia: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    accion: {
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+    fecha: {
+        type: Sequelize.DATEONLY,
+        allowNull: false
+    },
+    id_tabla_referencia: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+    }
+},{
+    timestamps: false,
+    tableName: 'bitacora'
+});
+
+export default Bitacora;
