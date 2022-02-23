3ad1659b2518910a9f0bceec4d9c63b93aa41271
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-23 20:47:57 +0000

    Upload tecnologia.js

---
+++ src/models/tecnologia.js
@@ -0,0 +1,26 @@
+import {Sequelize} from 'sequelize';
+import {sequelize} from '../database/db';
+import Aviso from './aviso';
+
+const Tecnologia = sequelize.define('tecnologia',{
+    id: {
+        type:Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre:{
+        type: Sequelize.STRING,
+        allowNull:false
+    }
+},{
+    timestamps: false,
+    tableName: 'tecnologia'
+});
+
+//Definicion relacion aviso
+
+Tecnologia.belongsToMany (Aviso,{through: "aviso_has_tecnologia"});
+Aviso.belongsToMany(Tecnologia,{through: "aviso_has_tecnologia"});
+
+
+export default Tecnologia;
