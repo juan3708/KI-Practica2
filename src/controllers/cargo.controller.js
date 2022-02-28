95983a03b7f4366b98ae5c6f04ec7ac1607e4fb8
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-28 18:09:55 +0000

    Replace cargo.controller.js

--- src/controllers/cargo.controller.js
+++ src/controllers/cargo.controller.js
@@ -1,42 +1,19 @@
-import Cargo from "../models/cargo";
+import {Sequelize} from 'sequelize';
+import {sequelize} from '../database/db';
 
-export async function crearCargo(req,res){
-    const {nombre} = req.body;
-    try{
-        let nuevoCargo = await Cargo.create({
-            nombre
-        })
-        if(nuevoCargo){
-            res.json ({
-                code:200,
-                message: 'Cargo creado con exito',
-                data: nuevoCargo
-            });
-        }
-    }catch (e){
-        res.json({
-            code:400,
-            message: 'Error al crear el cargo',
-            error: e.errors
-        });
+const Cargo = sequelize.define('cargo', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    nombre: {
+        type: Sequelize.STRING,
+        allowNull: false
     }
-}
+},{
+    timestamps: false,
+    tableName: 'cargo'
+});
 
-export async function listarCargo(req, res){
-    try{
-        let cargos = await Cargo.findAll({
-             attributes: ['id','nombre']
-        });
-        res.json({
-            code:200,
-            message: 'Estados Listados Con Exito',
-            cargos
-        });
-    }catch(e){
-        res.json({
-            code:400,
-            message: 'Error al listar los estados',
-            error: e
-        });
-    }
-}
+export default Cargo;
