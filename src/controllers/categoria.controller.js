a1e40f122981928515889a650bdb3871300332e6
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-28 18:11:42 +0000

    Upload categoria.controller

---
+++ src/controllers/categoria.controller.js
@@ -0,0 +1,110 @@
+import Categoria from "../models/categoria";
+
+export async function crearCategoria (req, res){
+    const {nombre} = req.body;
+    try{
+        let nuevaCategoria = await Categoria.create({
+            nombre
+        })
+        if(nuevaCategoria){
+            res.json({
+                code:200,
+                message: 'Categoria creada con exito',
+                data : nuevaCategoria
+            });
+        }
+    }catch(e){
+        res.json({
+            code:400,
+            message: 'Error al crear la categoria',
+            error: e.errors
+        });
+    }
+}
+
+export async function listarCategoria (req, res){
+    try{
+        let categorias = await Categoria.findAll({
+            attributes: ['id' , 'nombre']
+        });
+        res.json({
+            code:200,
+            message: 'Categorias listadas con exito',
+            categorias
+        });
+    }catch(e){
+        res.json({
+            code:400,
+            message: 'Error al listar las categorias',
+            error: e
+        });
+    }
+}
+
+export async function editarCategoria (req, res){
+    const{id,nombre} = req.body;
+    if(id && nombre){
+        try{
+            let categoria = await Categoria.findOne({
+                where:{id:id},
+                attributes: ['id' , 'nombre']
+            });
+            if(categoria){
+                categoria.update({nombre});
+                res.json({
+                    code:200,
+                    message: 'La categoria ha sido editada con exito',
+                    data: categoria
+                });
+            }else{
+                res.json({
+                    code:400,
+                    message: 'La categoria no existe'
+                });
+            }
+        }catch(e){
+            res.json({
+                code:401,
+                message :'Error'
+            });
+        }
+    }else{
+        res.json({
+            code:203,
+            message: 'No ha ingresado campos para editar'
+        });
+    }
+}
+export async function eliminarCategoria (req, res){
+    const{id,nombre} = req.body;
+    if(id && nombre){
+        try{
+            let cargo = await Categoria.findOne({
+                where: {id:id},
+                attributes: ['id','nombre']
+            });
+            if(cargo){
+                cargo.destroy({nombre});
+                res.json ({
+                    code:200,
+                    message: 'La categoria ha sido eliminada exitosamente',
+                });
+            }else{
+                res.json({
+                    code:400,
+                    message : 'La categoria no existe'
+                });
+            }
+        }catch(e){
+            res.json({
+                code:401,
+                message: 'ERROR'
+            });
+        }
+    }else{
+        res.json({
+            code:203,
+            message: 'No ha ingresado campos para eliminar'
+        });
+    }
+}
