93f5562a40e135bab4f496be170720d1a3347bc1
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-28 18:11:05 +0000

    Replace cargo.controller.js

--- src/controllers/cargo.controller.js
+++ src/controllers/cargo.controller.js
@@ -1,19 +1,111 @@
-import {Sequelize} from 'sequelize';
-import {sequelize} from '../database/db';
+import Cargo from "../models/cargo";
 
-const Cargo = sequelize.define('cargo', {
-    id: {
-        type: Sequelize.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-    },
-    nombre: {
-        type: Sequelize.STRING,
-        allowNull: false
+export async function crearCargo(req,res){
+    const {nombre} = req.body;
+    try{
+        let nuevoCargo = await Cargo.create({
+            nombre
+        })
+        if(nuevoCargo){
+            res.json ({
+                code:200,
+                message: 'Cargo creado con exito',
+                data: nuevoCargo
+            });
+        }
+    }catch (e){
+        res.json({
+            code:400,
+            message: 'Error al crear el cargo',
+            error: e.errors
+        });
     }
-},{
-    timestamps: false,
-    tableName: 'cargo'
-});
+}
 
-export default Cargo;
+export async function listarCargo(req, res){
+    try{
+        let cargos = await Cargo.findAll({
+             attributes: ['id','nombre']
+        });
+        res.json({
+            code:200,
+            message: 'Cargos listados Con Exito',
+            cargos
+        });
+    }catch(e){
+        res.json({
+            code:400,
+            message: 'Error al listar los cargos',
+            error: e
+        });
+    }
+}
+
+export async function editarCargo (req, res){
+    const {id, nombre} =req.body;
+    if(id && nombre){
+        try{
+            let cargo = await Cargo.findOne({
+                where: {id:id},
+                attributes: ['id','nombre']
+            });
+            if(cargo){
+                cargo.update({nombre});
+                res.json ({
+                    code:200,
+                    message: 'El cargo ha sido editado exitosamente',
+                    data: cargo
+                });
+            }else{
+                res.json({
+                    code:400,
+                    message : 'El cargo no existe'
+                });
+            }
+        }catch(e){
+            res.json({
+                code:401,
+                message: 'ERROR'
+            });
+        }
+    }else{
+        res.json({
+            code:203,
+            message: 'No ha ingresado campos para editar'
+        })
+    }
+}
+
+export async function eliminarCargo (req, res){
+    const{id,nombre} = req.body;
+    if(id && nombre){
+        try{
+            let cargo = await Cargo.findOne({
+                where: {id:id},
+                attributes: ['id','nombre']
+            });
+            if(cargo){
+                cargo.destroy({nombre});
+                res.json ({
+                    code:200,
+                    message: 'El cargo ha sido eliminado exitosamente',
+                });
+            }else{
+                res.json({
+                    code:400,
+                    message : 'El cargo no existe'
+                });
+            }
+        }catch(e){
+            res.json({
+                code:401,
+                message: 'ERROR'
+            });
+        }
+    }else{
+        res.json({
+            code:203,
+            message: 'No ha ingresado campos para eliminar'
+        });
+    }
+}
