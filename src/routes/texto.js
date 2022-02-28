7758585e7e2c9e278f5716dd7e062a855d54a7d5
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-02-28 18:18:19 +0000

    Update texto.js

--- src/routes/texto.js
+++ src/routes/texto.js
@@ -5,7 +5,7 @@ const router = Router();
 
 router.post('/crear', crearTexto);
 router.get('/listar', listarTexto);
-//router.put('/editar', editarTecnologia);
+//router.put('/editar', editarTexto);
 
 
 export default router;
