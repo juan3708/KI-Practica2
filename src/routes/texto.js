1e556f4e8e886856541115fe69ff2c44e5351d99
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-01 19:37:02 +0000

    Replace texto.js

--- src/routes/texto.js
+++ src/routes/texto.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import { crearTexto, listarTexto,editarTexto} from "../controllers/texto.controller";
+import { crearTexto, listarTexto, editarTexto, eliminarTexto} from "../controllers/texto.controller";
 
 const router = Router();
 
 router.post('/crear', crearTexto);
 router.get('/listar', listarTexto);
 router.put('/editar', editarTexto);
+router.delete('/eliminar', eliminarTexto);
 
 
 export default router;
