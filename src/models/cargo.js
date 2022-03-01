1e3f715531f8be2185f3fe5c6d6dab2ef75a7d90
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-01 19:07:51 +0000

    Replace cargo.js

--- src/models/cargo.js
+++ src/models/cargo.js
@@ -1,5 +1,6 @@
-import {Sequelize} from 'sequelize';
+import {DataTypes, Sequelize} from 'sequelize';
 import {sequelize} from '../database/db';
+import Aviso from '../models/aviso';
 
 const Cargo = sequelize.define('cargo', {
     id: {
@@ -10,10 +11,29 @@ const Cargo = sequelize.define('cargo', {
     nombre: {
         type: Sequelize.STRING,
         allowNull: false
+    },
+    operativo: {
+        type: DataTypes.BOOLEAN,
+        allowNull:false
     }
 },{
     timestamps: false,
     tableName: 'cargo'
+});
+
+//Definicion relacion con aviso
+
+Cargo.hasMany(Aviso,{
+    foreignKey:{
+        name: 'cargo_id',
+    },
+    sourceKey: 'id'
+})
+Aviso.belongsTo (Cargo, {
+    foreignKet: {
+        name: 'cargo_id',
+    },
+    sourceKey: 'id'
 });
 
 export default Cargo;
