d24f924cbdba8cf085569d4faa0a32e2fed51613
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-01 19:09:57 +0000

    Replace rol.js

--- src/routes/rol.js
+++ src/routes/rol.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { crearRol, listarRol, editarRol} from "../controllers/rol.controller";
+import { crearRol, listarRol, editarRol, eliminarRol} from "../controllers/rol.controller";
 
 const router = Router();
 
 router.post('/crear', crearRol);
 router.get('/listar', listarRol);
 router.put('/editar', editarRol);
-//router.delete('/eliminar', eliminarRol);
+router.delete('/eliminar', eliminarRol);
 
 export default router;
