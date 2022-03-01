551aaefa4d98b0802020446e971fe8f170cee244
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-01 19:05:53 +0000

    Replace categoria.controller.js

--- src/controllers/categoria.controller.js
+++ src/controllers/categoria.controller.js
@@ -1,19 +1,20 @@
 import Categoria from "../models/categoria";
 
-export async function crearCategoria (req, res){
-    const {nombre} = req.body;
+export async function crearCategoria(req,res){
+    const {nombre, operativo} = req.body;
     try{
         let nuevaCategoria = await Categoria.create({
-            nombre
+            nombre,
+            operativo
         })
         if(nuevaCategoria){
-            res.json({
+            res.json ({
                 code:200,
                 message: 'Categoria creada con exito',
-                data : nuevaCategoria
+                data: nuevaCategoria
             });
         }
-    }catch(e){
+    }catch (e){
         res.json({
             code:400,
             message: 'Error al crear la categoria',
@@ -22,10 +23,10 @@ export async function crearCategoria (req, res){
     }
 }
 
-export async function listarCategoria (req, res){
+export async function listarCategoria(req, res){
     try{
         let categorias = await Categoria.findAll({
-            attributes: ['id' , 'nombre']
+            attributes: ['id','nombre','operativo']
         });
         res.json({
             code:200,
@@ -35,56 +36,57 @@ export async function listarCategoria (req, res){
     }catch(e){
         res.json({
             code:400,
-            message: 'Error al listar las categorias',
+            message: 'Error al listar los categorias',
             error: e
         });
     }
 }
 
 export async function editarCategoria (req, res){
-    const{id,nombre} = req.body;
-    if(id && nombre){
+    const {id, nombre, operativo} =req.body;
+    if(id && nombre && operativo){
         try{
             let categoria = await Categoria.findOne({
-                where:{id:id},
-                attributes: ['id' , 'nombre']
+                where: {id:id},
+                attributes: ['id','nombre','operativo']
             });
             if(categoria){
-                categoria.update({nombre});
-                res.json({
+                categoria.update({nombre,operativo});
+                res.json ({
                     code:200,
-                    message: 'La categoria ha sido editada con exito',
+                    message: 'La categoria ha sido editada exitosamente',
                     data: categoria
                 });
             }else{
                 res.json({
                     code:400,
-                    message: 'La categoria no existe'
+                    message : 'la categoria no existe'
                 });
             }
         }catch(e){
             res.json({
                 code:401,
-                message :'Error'
+                message: 'ERROR'
             });
         }
     }else{
         res.json({
             code:203,
             message: 'No ha ingresado campos para editar'
-        });
+        })
     }
 }
+
 export async function eliminarCategoria (req, res){
-    const{id,nombre} = req.body;
-    if(id && nombre){
+    const{id,nombre, operativo} = req.body;
+    if(id && nombre && operativo){
         try{
-            let cargo = await Categoria.findOne({
+            let categoria = await Categoria.findOne({
                 where: {id:id},
-                attributes: ['id','nombre']
+                attributes: ['id']
             });
-            if(cargo){
-                cargo.destroy({nombre});
+            if(categoria){
+                await categoria.update({operativo:false});
                 res.json ({
                     code:200,
                     message: 'La categoria ha sido eliminada exitosamente',
