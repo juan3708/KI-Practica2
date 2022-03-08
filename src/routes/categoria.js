74e57aeae38a6e8bb407ec65dd363296cbd897de
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-08 21:18:20 +0000

    Replace categoria.js

--- src/routes/categoria.js
+++ src/routes/categoria.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { crearCategoria, listarCategoria, editarCategoria, eliminarCategoria} from "../controllers/categoria.controller";
+import { crearCategoria, listarCategoria, editarCategoria, eliminarCategoria,
+BuscarCategoriaPorId, BuscarCategoriaPorNombre, BuscarCategoriaPorOperatividad} from "../controllers/categoria.controller";
 
 const router = Router();
 
@@ -7,5 +8,7 @@ router.post('/crear', crearCategoria);
 router.get('/listar', listarCategoria);
 router.put('/editar', editarCategoria);
 router.delete('/eliminar', eliminarCategoria);
-
+router.get('/buscar/id', BuscarCategoriaPorId);
+router.get('/buscar/nombre', BuscarCategoriaPorNombre);
+router.get('/buscar/operativo', BuscarCategoriaPorOperatividad);
 export default router;
