11054423f055625ae2c48be8f66e427939084eea
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-08 21:17:32 +0000

    Replace aviso.js

--- src/routes/aviso.js
+++ src/routes/aviso.js
@@ -1,10 +1,14 @@
 import { Router } from "express";
-import { crearAviso, listarAvisos} from "../controllers/aviso.controller";
+import { crearAviso, listarAvisos, editarAviso, BuscarAvisosPorEstado, BuscarAvisoPorTitulo, BuscarAvisoPorId, BuscarAvisoPorCargo} from "../controllers/aviso.controller";
 
 const router = Router();
 
 router.post('/crear', crearAviso);
 router.get('/listar', listarAvisos);
-
+router.put('/editar', editarAviso);
+router.get('/buscar/estado', BuscarAvisosPorEstado);
+router.get('/buscar/titulo', BuscarAvisoPorTitulo);
+router.get('/buscar/id', BuscarAvisoPorId);
+router.get('/buscar/cargo', BuscarAvisoPorCargo);
 
 export default router;
