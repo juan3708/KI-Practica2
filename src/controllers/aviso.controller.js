bc77983dc6726cf796f5dfd7d8f50b094e49ebfd
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-08 21:10:19 +0000

    Replace aviso.controller.js

--- src/controllers/aviso.controller.js
+++ src/controllers/aviso.controller.js
@@ -50,3 +50,186 @@ export async function listarAvisos(req, res){
         });
     }
 }
+
+export async function editarAviso (req, res){
+    const {id, titulo,
+        funciones,
+        rango_inicial,
+        rango_final,
+        estado
+        } =req.body;
+    if(id && titulo && funciones && rango_inicial && rango_final && estado){
+        try{
+            let aviso = await Aviso.findOne({
+                where: {id:id},
+                include:[{
+                    model: Cargo,
+                    attributes: ['id']
+                }],
+                attributes: ['id', 'titulo',
+                    'funciones',
+                    'rango_inicial',
+                    'rango_final',
+                    'estado']
+            });
+            if(aviso){
+                aviso.update({id, titulo, funciones, rango_inicial, rango_final, estado});
+                res.json ({
+                    code:200,
+                    message: 'El aviso ha sido editado exitosamente',
+                    data:aviso
+                });
+            }else{
+                res.json({
+                    code:400,
+                    message : 'El aviso no existe'
+                });
+            }
+        }catch(e){
+            res.json({
+                code:401,
+                message: 'ERROR'
+            });
+        }
+    }else{
+        res.json({
+            code:203,
+            message: 'No ha ingresado campos para editar'
+        })
+    }
+}
+
+export async function BuscarAvisoPorId(req, res) {
+    const {id} = req.body;
+    try{
+        let aviso = await Aviso.findOne({
+            where: {id:id},
+            attributes: ['id','titulo',
+                'funciones',
+                'rango_inicial',
+                'rango_final',
+                'estado', 
+            ]
+        });
+        if(aviso){
+            res.json({
+                code: 200,
+                message: 'Se ha encontrado el aviso',
+                data: aviso
+            });
+        } else {
+            res.json({
+                code: 400,
+                message: 'El aviso con ese id no existe'
+            });
+        }
+    } catch (e) {
+        res.json({
+            code: 401,
+            message: 'ERROR'
+        });
+    }
+}
+
+//FALTA VALIDACIÓN 
+export async function BuscarAvisoPorTitulo(req, res) {
+    const {titulo} = req.body;
+    try{
+        let aviso = await Aviso.findOne({
+            where: {titulo:titulo},
+            attributes: ['id','titulo',
+                'funciones',
+                'rango_inicial',
+                'rango_final',
+                'estado', 
+            ]
+        });
+        if(aviso){
+            res.json({
+                code: 200,
+                message: 'Se ha encontrado el aviso',
+                data: aviso
+            });
+        } else {
+            res.json({
+                code: 400,
+                message: 'El aviso no existe'
+            });
+        }
+    } catch (e) {
+        res.json({
+            code: 401,
+            message: 'ERROR'
+        });
+    }
+}
+
+export async function BuscarAvisosPorEstado(req, res) {
+    const {estado} = req.body;
+    try{
+        let avisos = await Aviso.findAll({
+            where: {estado:estado},
+            attributes: ['id','titulo',
+                'funciones',
+                'rango_inicial',
+                'rango_final',
+                'estado']
+        });
+        if(avisos){
+            res.json({
+                code:200,
+                message: 'Los avisos han sido encontrados con exito',
+                data : avisos
+            });
+        }else{
+            res.json({
+                code:400,
+                message: 'El aviso no existe'
+            });
+        }
+    }catch(e){
+        res.json({
+            code:401,
+            message: 'ERROR'
+        });
+    }
+}
+
+export async function BuscarAvisoPorCargo(req, res) {
+    const {id} = req.body;
+    try{
+    let avisos = await Aviso.findAll({
+        where: {cargo_id:id},
+        include: [{
+            model: Cargo,
+            attributes: ['nombre']
+        }],
+        attributes: ['id', 'titulo',
+        'funciones',
+        'rango_inicial',
+        'rango_final',
+        'estado']
+        });
+        if(avisos){
+            res.json({
+                code: 200,
+                message: 'Avisos listadas correctamente',
+                data: avisos
+            });
+        } else {
+            res.json({
+                code: 400,
+                message: 'No existe el cargo',
+            });
+        }
+    } catch (e) {
+        res.json({
+            code: 401,
+            message: 'Error',
+
+        });
+    }
+}
+
+
+
