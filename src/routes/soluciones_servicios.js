83f792b514cd9af1360f604b5411fdfe6a71b138
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-09 20:24:30 +0000

    Replace soluciones_servicios.js

--- src/routes/soluciones_servicios.js
+++ src/routes/soluciones_servicios.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
-import { crearSoluciones_servicios, listarSoluciones_servicios, editarSoluciones_servicios, eliminarSoluciones_servicios, 
-BuscarPorTag} from "../controllers/soluciones_servicios";
+import { crearSoluciones_servicios, listarSoluciones_servicios, editarSoluciones_servicios, 
+    eliminarSoluciones_servicios,  BuscarPorTag, BuscarPorId, BuscarPorTipo,
+    BuscarPorNombre, BuscarPorTitulo, BuscarPorOperatividad} from "../controllers/soluciones_servicios.controller";
 
 const router = Router();
 
@@ -8,6 +9,11 @@ router.post('/crear', crearSoluciones_servicios);
 router.get('/listar', listarSoluciones_servicios);
 router.put('/editar', editarSoluciones_servicios);
 router.delete('/eliminar', eliminarSoluciones_servicios);
+router.get('/buscar/id', BuscarPorId);
+router.get('/buscar/tipo', BuscarPorTipo);
+router.get('/buscar/nombre', BuscarPorNombre);
 router.get('/buscar/tag', BuscarPorTag);
+router.get('/buscar/titulo', BuscarPorTitulo);
+router.get('/buscar/operatividad', BuscarPorOperatividad);
 
 export default router;
