e58c369fcf129135a8dd079aa42655e185d83332
Catalina Andrea Fernández <cfernandez@kiteknology.com>
2022-03-09 20:24:04 +0000

    Replace postulante.js

--- src/routes/postulante.js
+++ src/routes/postulante.js
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { crearPostulante, listarPostulante, editarPostulante, eliminarPostulante, BuscarPostulantePorId, BuscarPostulantePorCorreo} from "../controllers/postulante.controller";
+import { crearPostulante, listarPostulante, editarPostulante, eliminarPostulante, BuscarPostulantePorId, BuscarPostulantePorCorreo, 
+    BuscarPostulantePorNombre, BuscarPostulantePorApellido, BuscarPostulantePorExperiencia, BuscarPostulantePorOperatividad} from "../controllers/postulante.controller";
 
 const router = Router();
 
@@ -8,6 +9,10 @@ router.get('/listar', listarPostulante);
 router.put('/editar', editarPostulante);
 router.delete('/eliminar', eliminarPostulante);
 router.get('/buscar/id', BuscarPostulantePorId);
+router.get('/buscar/nombre', BuscarPostulantePorNombre);
+router.get('/buscar/apellido', BuscarPostulantePorApellido);
 router.get('/buscar/correo', BuscarPostulantePorCorreo);
+router.get('/buscar/experiencia', BuscarPostulantePorExperiencia);
+router.get('/buscar/operatividad', BuscarPostulantePorOperatividad);
 
 export default router;
